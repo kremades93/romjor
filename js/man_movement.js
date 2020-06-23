3d4a634809ae98bbeca256aa7a72fa4fece4ba47
MiPc <MiPc@10.0.0.90>
2020-06-22 19:01:26 -0500

    Merge origin/master
    Conflicts:
    	public_html/index.html

--- js/man_movement.js
+++ js/man_movement.js
@@ -6,7 +6,7 @@ function echsel(txt = "") {
 }
 
 //<!-- Moviment dels little men -->
-function movepers(x) {
+function movepers0(x) {
     if(selected != "") { //this means the human is already selected
         if(x.className==="earth" && x.id != notsel) {
             prevtile.innerHTML="";
@@ -27,5 +27,24 @@ function movepers(x) {
 function movepers2(x,posh1){
     if (posh2=="3-5"){
         x.innerHTML=humanhtml;
+    }
+}
+
+//Romà 22.06.2020: he modificat la funció movepers() i  a la original la he anomenat movepers0()
+function movepers(x) {
+    if(selected != "") { //this means the human is already selected
+        if(x.className==="earth" && x.id != posh2) {
+            prevtile.innerHTML="";
+            x.innerHTML=jug1html;
+            document.getElementById("cara1").innerHTML =":)";
+            selected="", notsel="";
+            posh1 = x.id;
+        }
+        document.getElementById("echo").innerHTML = "class:" + x.className + " id: " + x.id;
+    } else if ( x.id===posh1 ){
+        if(x.id===posh1) {selected=posh1; notsel=posh2}
+        x.innerHTML = jug1html;
+        document.getElementById("cara1").innerHTML =":D";
+        prevtile=x;
     }
 }
