0ce3a80ed763bf0ac22e609e5499fd978388fd01
kremades93 <jcremades93@gmail.com>
2020-06-24 19:57:16 +0200

    -initial parameters ja te un .js

---
+++ js/initial_parameters.js
@@ -0,0 +1,7 @@
+var selected = "";
+var notsel="";
+var prevtile="";
+var humanhtml='<div id="jugador1"><span class="leg1"></span><span class="leg2"></span><span class="arms"></span><span class="body"></span><span class="head"><div id="cara1" class="cara">:)</div></span></div>';
+var happyhuman='<div id="jugador1"><span class="leg1"></span><span class="leg2"></span><span class="arms"></span><span class="body"></span><span class="head"><div id="cara1" class="cara">:D</div></span></div>';
+var posh1="1-4";
+var posh2="3-5";
--- js/man_movement.js
+++ js/man_movement.js
@@ -2,10 +2,16 @@
 // Aquesta funció quina funcio té? --> 
 // Romà: ni idea, només era per provar d'ensenyar el valor d'unes variables
 function echsel(txt = "") {
-   document.getElementById("echo").innerHTML = txt + ".selected=" + selected + ".";
+   document.getElementById("position_man1").innerHTML = txt + ".selected=" + selected + ".";
 }
 
-//<!-- Moviment dels little men -->
+function movepers2(x,posh1){
+    if (posh2=="3-5"){
+        x.innerHTML=humanhtml;
+    }
+}
+
+//Aquesta funció ha quedat obsoleta!!!!
 function movepers0(x) {
     if(selected != "") { //this means the human is already selected
         if(x.className==="earth" && x.id != notsel) {
@@ -15,7 +21,7 @@ function movepers0(x) {
             if(prevtile.id===posh1) posh1 = x.id;
             if(prevtile.id===posh2) posh2 = x.id;
         }
-        document.getElementById("echo").innerHTML = "class:" + x.className + " id: " + x.id;
+        document.getElementById("position_man1").innerHTML = "class:" + x.className + " id: " + x.id;
     } else if ( x.id===posh1 || x.id===posh2 ){
         if(x.id===posh1) {selected=posh1; notsel=posh2}
         if(x.id===posh2) {selected=posh2; notsel=posh1}
@@ -24,15 +30,10 @@ function movepers0(x) {
     }
 }
 
-function movepers2(x,posh1){
-    if (posh2=="3-5"){
-        x.innerHTML=humanhtml;
-    }
-}
-
 //Romà 22.06.2020: he modificat la funció movepers() i  a la original la he anomenat movepers0()
+//Jordi: no s'utilitza aquesta funció ara... s'utilitza la movepers0
 function movepers(x) {
-    if(selected != "") { //this means the human is already selected
+    if(selected != "") { //this means the user has selected the human
         if(x.className==="earth" && x.id != posh2) {
             prevtile.innerHTML="";
             x.innerHTML=jug1html;
@@ -40,7 +41,7 @@ function movepers(x) {
             selected="";
             posh1 = x.id;
         }
-        document.getElementById("echo").innerHTML = "class:" + x.className + " id: " + x.id;
+        document.getElementById("mambo1").innerHTML = "class:" + x.className + " id: " + x.id;
     } else if ( x.id===posh1 ){
         if(x.id===posh1) {selected=posh1;}
         x.innerHTML = jug1html;
