49d63eb52a08d4558ec9b542dd1fa5d5b0ca8c10
MiPc <MiPc@10.0.0.90>
2020-06-26 02:49:28 -0500

    Merge origin/master
    Conflicts:
    	public_html/js/function/movement.js

--- js/man_movement.js
+++ js/man_movement.js
@@ -38,7 +38,8 @@ function movepers(x) {
             document.getElementById("cara1").innerHTML =":)";
             selected="";
             posh1 = x.id;
-            document.getElementById("mambo2").innerHTML =posh1;            
+            document.getElementById("mambo2").innerHTML =posh1;
+            finishturn();
 
         }
         document.getElementById("mambo1").innerHTML = "class:" + x.className + " id: " + x.id;
